e4558225fa363c3e2a61e442c9d8919a58bdec40
kristosh <chrathans@gmail.com>
2025-02-19 17:09:20 +0100

    xai_updates

--- feature_attribution.py
+++ feature_attribution.py
@@ -7,7 +7,7 @@ inseq.list_feature_attribution_methods()
 
 # load a model from HuggingFace model hub and define the feature attribution 
 # method you want to use
-mdl_gpt2 = inseq.load_model("gpt2", "integrated_gradients")
+mdl_gpt2 = inseq.load_model("gpt2", "lime")
 
 # compute the attributions for a given prompt
 attr = mdl_gpt2.attribute(
